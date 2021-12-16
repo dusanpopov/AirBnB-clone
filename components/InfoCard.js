f9cf59888878955eb69778e7414e9e5f245b89e7
Dušan Popov <aristocut90@gmail.com>
2021-12-16 15:29:04 +0100

    Update InfoCard.js

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -17,7 +17,6 @@ const InfoCard = ({img, location, title, description, star, price, total}) => {
                 <h4 className="text-xl">{title}</h4>
                 <div className="border-b w-10 pt-2"/>
                 <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
-
                 <div className="flex justify-between items-end pt-5">
                     <p className="flex items-center"><StarIcon className="h-5 text-red-400" />{star}</p>
                     <div>
