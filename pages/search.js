b0abc6f382c65e3221a7ea38c7d7e5ff7a2e66a4
Dušan Popov <aristocut90@gmail.com>
2022-01-17 08:40:30 +0100

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -48,7 +48,6 @@ const Search = ({searchResults}) => {
                         ))}
                         </div>
                     </section> 
-
                     <section className="hidden xl:inline-flex xl:min-w-[600px]">
                         <Map searchResults={searchResults}/>
                     </section>  
