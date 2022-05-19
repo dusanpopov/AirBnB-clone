fef6b66ce98f54d98719384ba29e66953613d8b5
Dušan Popov <aristocut90@gmail.com>
2022-05-19 03:55:46 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -45,14 +45,12 @@ const Header = ({placeholder}) => {
             <div onClick={() => router.push("/")} className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image src="https://links.papareact.com/qd3" layout="fill" objectFit="contain" objectPosition="left"/>
             </div>
-
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
                 <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)} type="text" placeholder="Start your search" 
                 className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400" placeholder={placeholder || "Start your search"}
                  />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
-
             <div className="flex items-center justify-end space-x-4 text-gray-500">
                 <p className="hidden md:inline cursor-pointer">Become a host</p>
                 <GlobeAltIcon className="h-6 cursor-pointer"/>
@@ -70,7 +68,6 @@ const Header = ({placeholder}) => {
                     rangeColors={["#FD5B61"]}
                     onChange={handleDateSelect}
                  />
-
                 <div className="flex items-center border-b mb-4">
                     <h2 className="text-2xl flex-grow font-semibold">Number of guests</h2>
                     <UsersIcon className="h-5" />
